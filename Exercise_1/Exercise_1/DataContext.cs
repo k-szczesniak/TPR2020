9eb943d4e40237daf425b7e6d72a22fccc421292
Dominik Kasierski <dominek19@wp.pl>
2020-11-06 15:33:49 +0100

    Added DataContext

---
+++ Exercise_1/Exercise_1/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Exercise_1
+{
+    class DataContext
+    {
+        public List<User> Users { get; set; } = new List<User>();
+        public Dictionary<int, Catalog> Catalogs { get; set; } = new Dictionary<int, Catalog>();
+        public ObservableCollection<Event> Events { get; set; } = new ObservableCollection<Event>();
+        public List<State> States { get; set; } = new List<State>();
+    }
+}
--- Exercise_1/Exercise_1/Event.cs
+++ Exercise_1/Exercise_1/Event.cs
@@ -1,14 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Exercise_1
 {
     class Event
     {
+        public string Id { get; set; }
         public User User { get; set; }
         public State State { get; set; }
         public DateTime RentalDate { get; set; }
         public DateTime GiveBackDate { get; set; }
+
+        public Event(string id, User user, State state, DateTime rentalDate, DateTime giveBackDate)
+        {
+            Id = id;
+            User = user;
+            State = state;
+            RentalDate = rentalDate;
+            GiveBackDate = giveBackDate;
+        }
     }
 }
