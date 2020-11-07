de6636091f14c26fc48d68d18e73b307eaad2778
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2020-11-07 13:41:49 +0100

    Fill of User, Catalog, States implementation

--- Exercise_1/Exercise_1/Filler.cs
+++ Exercise_1/Exercise_1/Filler.cs
@@ -6,9 +6,32 @@ namespace Exercise_1
 {
     public class Filler : IFiller
     {
-        public void Fill(string path)
+        public void Fill(DataContext context)
         {
-            //TODO: Miejsce na metodę która wczytuje z pliku
+            #region User
+            context.Users.Add(new User("Jan", "Kowalski"));
+            context.Users.Add(new User("Janusz", "Nowak"));
+            context.Users.Add(new User("Anna", "Nowak"));
+            context.Users.Add(new User("Michał", "Kwiatkowski"));
+            context.Users.Add(new User("Violetta", "Michniewska"));
+            #endregion
+
+            #region Catalogs
+            context.Catalogs.Add(1, new Catalog("Bolasław Prus", "Lalka"));
+            context.Catalogs.Add(2, new Catalog("Stanisław Wyspiański", "Wesele"));
+            context.Catalogs.Add(3, new Catalog("Juliusz Słowacki", "Balladyna"));
+            context.Catalogs.Add(4, new Catalog("Adam Mickiewicz", "Dziady"));
+            context.Catalogs.Add(5, new Catalog("Albert Camus", "Dżuma"));
+            #endregion
+
+            #region States
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                context.States.Add(new State(context.Catalogs[i + 1], "Book description " + i, 1, DateTime.Today.AddDays(-rand.Next(365))));
+            }
+            #endregion
+
         }
     }
 }
