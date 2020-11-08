6961138b3425cafe513d25852272de7329834ae6
Dominik Kasierski <dominek19@wp.pl>
2020-11-08 15:24:59 +0100

    Added tests for Filler

---
+++ Exercise_1/Exercise_1Tests/FillerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exercise_1.Tests
+{
+    [TestClass()]
+    public class FillerTests
+    {
+        [TestMethod()]
+        public void FillTest()
+        {
+            IFiller filler = new Filler();
+            DataContext dataContext = new DataContext();
+
+            filler.Fill(dataContext);
+
+            Assert.AreEqual(dataContext.Users.Count, 5);
+            Assert.AreEqual(dataContext.Catalogs.Count, 5);
+            Assert.AreEqual(dataContext.States.Count, 5);
+            Assert.AreEqual(dataContext.Events.Count, 10);
+        }
+    }
+}
