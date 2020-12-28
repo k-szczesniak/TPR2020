1cef02430ccba67242582ca5a19a2d99aedf2686
Dominik Kasierski <dominek19@wp.pl>
2020-12-28 16:23:40 +0100

    MyProduct, MyProductDataContext, MyProductSqlToolClass added

---
+++ Exercise_3/Exercise_3/MyProductDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    public class MyProductDataContext
+    {
+        public static List<MyProduct> MyProductsList { get; private set; }
+
+        public MyProductDataContext(ProductionDataContext myDataContext)
+        {
+            MyProductsList = myDataContext.Products.AsEnumerable().Select(product => new MyProduct(product)).ToList();
+        }
+    }
+}
+    
---
+++ Exercise_3/Exercise_3/MyProductSqlToolClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    public class MyProductSqlToolClass
+    {
+        public static List<MyProduct> GetMyProductsByName(string namePart)
+        {
+            List<MyProduct> productsList = new List<MyProduct>(from product in MyProductDataContext.MyProductsList
+                                                               where product.Name.Contains(namePart)
+                                                               select product);
+            return productsList;
+        }
+
+        public static List<MyProduct> GetMyProductsWithNRecentReviews(int howManyReviews)
+        {
+            List<MyProduct> productsList = new List<MyProduct>(from product in MyProductDataContext.MyProductsList
+                                                               where product.ProductReviews.Count == howManyReviews
+                                                               select product);
+            return productsList;
+        }
+
+        public static int GetTotalStandardCostByCategory(ProductCategory category)
+        {
+            int totalStandardCost = (int)(from product in MyProductDataContext.MyProductsList
+                                          where product.ProductSubcategory.ProductCategory.Name == category.Name
+                                          select product.StandardCost).Sum();
+            return totalStandardCost;
+        }
+    }
+}
