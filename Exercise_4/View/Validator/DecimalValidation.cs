770bf8bfad4ac42482cc122d028f526e1a61e12f
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-01-17 18:21:17 +0100

    Validation added

---
+++ Exercise_4/View/Validator/DecimalValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace View.Validator
+{
+    public class DecimalValidation : ValidationRule
+    {
+        public string Error { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string s = ((string)value);
+            decimal d = 0.00m;
+            try
+            {
+                d = Decimal.Parse(s, cultureInfo);
+            }
+            catch (Exception)
+            {
+                return new ValidationResult(false, $"Enter non negative decimal");
+            }
+
+            if (d >= 0.00m)
+            {
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, $"Enter non negative decimal");
+            }
+        }
+    }
+}
---
+++ Exercise_4/View/Validator/StringValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace View.Validator
+{
+    public class StringValidation : ValidationRule
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string s = ((string)value);
+            if (s.Length >= Min && s.Length <= Max)
+            {
+                return ValidationResult.ValidResult;
+            }
+            else
+            {
+                return new ValidationResult(false, $"Validation Error");
+            }
+        }
+    }
+}
