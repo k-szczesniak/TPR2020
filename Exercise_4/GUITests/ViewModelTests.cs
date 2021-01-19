7515b6350a0bf3d3aec7eab593f71c905d7473b8
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-01-19 18:10:31 +0100

    Removed unused using

--- Exercise_4/GUITests/ViewModelTests.cs
+++ Exercise_4/GUITests/ViewModelTests.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model;
 using Service;
+using System;
 using ViewModel;
 
 namespace GUITests
